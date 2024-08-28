3023e3c184047be697a3c567dd7ad6948ec58625
muhammed-arshid <muhammed.arshid@calpinetech.com>
2024-08-28 17:59:17 +0530

    learing day-1

---
+++ CPP/input.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+// #include<bits/stdc++.h>  // it includes all the library in cpp
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << "value of x" << x <<"Value of y:"<<y;
+    return 0;
+
+}
